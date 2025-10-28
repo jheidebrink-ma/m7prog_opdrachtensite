80aa668d8b50ab64091fa37f123f58c164d5f65f
progsen <>
2025-10-28 11:44:43 +0100

    faster files

---
+++ les-1/_faster laravel/install.sh
@@ -0,0 +1,34 @@
+wsl 
+cd /home/android
+mkdir lara
+cd lara
+
+in code \\wsl.localhost\Ubuntu\home\android\lara
+
+sudo apt install php8.1-cli
+curl -s https://laravel.build/m7portfolio | bash
+
+dan in WSL sail up
+//zet uit
+//dan container weer aan (via docker)
+dan in wsl sail migrate (heeft de container nodig)
+in de container:
+
+
+composer require laravel/breeze --dev
+
+php artisan breeze:install
+
+ 
+
+php artisan migrate
+
+npm install
+
+npm run dev - > error wrong version
+
+ROUTING:
+don't forget your namespaces
+
+
+npm install tailwindcss @tailwindcss/vite
